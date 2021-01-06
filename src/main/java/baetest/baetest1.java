93fb9673baa74d85078d077cbc64d8f6c7901e9e
Ulr <Ulr@LAPTOP-4U3PCS6V>
2021-01-06 12:53:31 +0900

    4교시 커밋

---
+++ src/main/java/baetest/baetest1.java
@@ -0,0 +1,20 @@
+package baetest;
+
+public class baetest1 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 10;
+		
+		x++;
+		++y;
+		
+		System.out.println(x++);
+		System.out.println(++y);
+		int result = x++ + ++y; 
+		System.out.println(result);
+		System.out.println(x);
+		System.out.println(y);
+		}
+
+}
