57704dd7330c87eb13b19ca34de9a0ff4c63308c
Ulr <Ulr@LAPTOP-4U3PCS6V>
2021-01-26 09:48:46 +0900

    1교시 커밋

--- src/main/java/org/edu/controller/AdminController.java
+++ src/main/java/org/edu/controller/AdminController.java
@@ -56,6 +56,12 @@ public class AdminController {
 	
 	@Inject
 	private IF_BoardTypeService boardTypeService;
+	//게시판생성관리 등록매핑(GET)
+	@RequestMapping(value="admin/bbs_type/bbs_type_write",method=RequestMethod.GET)
+	public String bbs_type_write() throws Exception {
+		
+		return "admin/bbs_type/bbs_type_write";
+	}
 	
 	//게시판생성관리 수정매핑(POST)
 	@RequestMapping(value="/admin/bbs_type/bbs_type_update",method=RequestMethod.POST)
